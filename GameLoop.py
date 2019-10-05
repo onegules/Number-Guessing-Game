fa62ed8ab88abc4b3f0667779e718ad02bf9a125
Seryph <Chrono.hi@gmail.com>
2019-10-05 12:46:18 -0400

    feat: Adds the explanation for how the game is played

---
+++ GameLoop.py
@@ -0,0 +1,7 @@
+print("In this game, the program will choose a number and your job is to guess")
+print("the number. You have 5 guesses and at each guess the computer will tell")
+print("you if the number you guesed is above or below the number that was picked.")
+print("The number picked will be between 1 and 100. Would you like to raise the")
+print("highest possible value?")
+
+for i in range(5):
