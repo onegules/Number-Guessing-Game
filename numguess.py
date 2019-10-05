7ddfc966917e692d2f25959e90df72b7d01eea92
Seryph <Chrono.hi@gmail.com>
2019-10-04 21:40:36 -0400

    refactor: Initialize the NumberGuess class without the guess argument.
    Initializing the class each time with the guess of the user forces
    new classes to be made per guess. This isn't very helpful in tracking
    if the guess is too high or too low so initializing the class with an
    optional "high" value made more sense. Now the user can decide to what
    the highest number that the computer can generate.

--- numguess.py
+++ numguess.py
@@ -1,10 +1,10 @@
 import computerguess
 
 class NumberGuess:
-    def __init__(self, guess):
-    ''' Initialize the function with the guess of the user'''
-        self.guess = guess
-
+    def __init__(self,high=100):
+    ''' Initialize the function and generates a random number based on the
+    optional argument'''
+        number = np.random.randint(1,high=high)
     def high(self):
 
     def low(self):
