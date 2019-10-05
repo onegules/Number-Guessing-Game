d463266f2759bca22c4cee1a7c0c93bf30ca0ad0
Seryph <Chrono.hi@gmail.com>
2019-10-04 22:00:42 -0400

    fix: Remove import computerguess.py

--- numguess.py
+++ numguess.py
@@ -1,10 +1,8 @@
-import computerguess
-
 class NumberGuess:
     def __init__(self,high=100):
         ''' Initialize the function and generates a random number based on the
         optional argument'''
-        
+
         number = np.random.randint(1,high=high)
 
     def result(self,guess):
