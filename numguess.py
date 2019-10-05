f94a085914c3e6bd5b86e9ab5ff046a3dbb838f2
Seryph <Chrono.hi@gmail.com>
2019-10-04 21:59:36 -0400

    doc: Document the result function

--- numguess.py
+++ numguess.py
@@ -2,11 +2,14 @@ import computerguess
 
 class NumberGuess:
     def __init__(self,high=100):
-    ''' Initialize the function and generates a random number based on the
-    optional argument'''
+        ''' Initialize the function and generates a random number based on the
+        optional argument'''
+        
         number = np.random.randint(1,high=high)
 
     def result(self,guess):
+        '''Calculates the result of a guess'''
+
         if guess < number:
             return "Too low, try again."
         elif guess > number:
