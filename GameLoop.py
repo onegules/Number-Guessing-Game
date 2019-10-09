81c230d1abfc2695f18b815c360f38dc7bc6ddb8
Seryph <Chrono.hi@gmail.com>
2019-10-09 19:05:10 -0400

    feat: Adds functionality that allows the user to change the max value

--- GameLoop.py
+++ GameLoop.py
@@ -4,10 +4,29 @@ print("In this game, the program will choose a number and your job is to guess")
 print("the number. You have 5 guesses and at each guess the computer will tell")
 print("you if the number you guesed is above or below the number that was picked.")
 print("The number picked will be between 1 and 100. Would you like to raise the")
-print("highest possible value?")
+print("highest possible value? [y/n]")
+
+answer = input()
+
+if(answer == 'y'):
+    print("What would you like the new highest value to be?")
+    try:
+        new_high = int(input())
+        numguess = NumberGuess(high=new_high)
+    except ValueError:
+        print("\nSeems you put in something I didn't understand. So I'll just initialize as the default\n")
+        numguess = NumberGuess()
+
+elif(answer == 'n'):
+    print("Alright then, let's start the game!")
+    numguess = NumberGuess()
+
+else:
+    print("\nSince I'm not sure what you typed, I will initialize using the default settings.\n")
+    numguess = NumberGuess()
 
 guesses = 5
-numguess = NumberGuess()
+
 for i in range(guesses):
     print("Please take a guess: ")
     guess = input()
