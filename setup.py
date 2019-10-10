4167e016f4950b3e616412ab9afed22b1961b10d
Seryph <Chrono.hi@gmail.com>
2019-10-10 19:56:01 -0400

    feat: Adds setup file for the package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='NumberGuessGame',
+        version='0.1',
+        description='Guessing game with customization',
+        packages=['NumberGuess'],
+        zip_safe=False)
