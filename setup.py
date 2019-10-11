9b1d29063bb93b56316f10700aaf0b7045736fd5
Seryph <Chrono.hi@gmail.com>
2019-10-10 20:10:49 -0400

    fix: Fixes name of folder

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='NumberGuessGame',
         version='0.1',
         description='Guessing game with customization',
-        packages=['NumberGuess'],
+        packages=['NumberGuessGame'],
         zip_safe=False)
