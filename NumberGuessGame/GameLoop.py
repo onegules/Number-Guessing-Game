cbe8fcdd537046d40d89b4525ac383f2ec828913
Seryph <Chrono.hi@gmail.com>
2019-10-11 17:50:07 -0400

    fix: Fixes class refactor errors

--- NumberGuessGame/GameLoop.py
+++ NumberGuessGame/GameLoop.py
@@ -13,21 +13,22 @@ class Game(NumberGuess):
         print("The number picked will be between 1 and 100. \n")
 
         print("To change any of the default values to customize your game type")
-        print("object_name.changeguess(your_guess) and changehigh(new_high)")
+        print("object_name.change_guesses(your_guess) and change_high(new_high)")
         print("to change the number of guesses in a game or the highest possible")
         print("value the computer will pick (still choosing from 1 to new_high).\n")
 
         print("To start the game run object_name.game_start(). Have fun!\n")
 
-    def changeguess(self,new_guess):
+    def change_guesses(self,new_guess):
         self.guesses = new_guess
 
-    def changehigh(self,new_high):
+    def change_high(self,new_high):
         self.high = new_high
 
     def game_start(self):
-        for i in range(guesses):
-            print("Please take a guess ({} left): ".format(guesses-i))
+        numguess = NumberGuess(self.high)
+        for i in range(self.guesses):
+            print("Please take a guess ({} left): ".format(self.guesses-i))
             guess = input()
             while True:
                 try:
@@ -41,7 +42,7 @@ class Game(NumberGuess):
                 print("Congratulations! You guessed right!")
                 break
 
-            elif(i == guesses-1):
+            elif(i == self.guesses-1):
                 result = numguess.result(int(guess))
                 print(result)
                 print("Better luck next time!")
